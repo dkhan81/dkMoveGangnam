f137b610b3942613a138c083d08ee73811f5c28a
한동규(Dongkyu Han)/Data Engineering팀/SKP <dkhan@sk.com>
2018-04-30 15:00:05 +0900

    test

--- gangnamapt/simple/views.py
+++ gangnamapt/simple/views.py
@@ -3,6 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+'''
 def hello(request) :
     html = "<h1>Hello, Django</h1>"
     return HttpResponse(html)
+'''
+
+def hello(request) :
+    people = {'name' : 'dk', 'age' : 20}
+    return render(request, 'simple/hello.html', {'data' : people})
