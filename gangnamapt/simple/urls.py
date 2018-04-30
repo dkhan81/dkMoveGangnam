6274ac654e0c64f0f6221d0a8403ef4dcabd7a02
한동규(Dongkyu Han)/Data Engineering팀/SKP <dkhan@sk.com>
2018-04-30 14:50:18 +0900

    2nd test

--- gangnamapt/simple/urls.py
+++ gangnamapt/simple/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('hello/', views.hello, name='hello'),
+    path('', views.hello, name='hello'),
 ]
