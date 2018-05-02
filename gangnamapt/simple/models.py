acf9e3c6055cf3a919f4c09a1a1d7270b07d17e3
한동규(Dongkyu Han)/Data Engineering팀/SKP <dkhan@sk.com>
2018-05-02 16:31:19 +0900

    model test

--- gangnamapt/simple/models.py
+++ gangnamapt/simple/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+from django.utils import timezone
+
+class DkNote(models.Model) :
+    name = models.CharField(max_length=30)
+    text = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+
+class Pk(models.Model) :
+    dk_id = models.CharField(max_length=30, primary_key=True)
+    name = models.CharField(max_length=30)
+    text = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
